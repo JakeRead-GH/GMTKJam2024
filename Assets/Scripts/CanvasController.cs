a9f2d1d45ba07fabfe7a914c8246039c98fdf30e
GabrielSiewert <gsiewert@conestogac.on.ca>
2024-08-18 16:54:27 -0400

    Edited template level

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -7,25 +7,49 @@ using UnityEngine.UI;
 
 public class CanvasController : MonoBehaviour
 {
-    private List<Slider> sliders = new List<Slider>();
+
+    private List<Slider> sliders;
+
+    private GameObject activeSlider;
+
+    // Camera is used to find the on screen position of the player
+    [SerializeField] private Camera levelCamera;
 
     // Start is called before the first frame update
     private void Awake()
     {
-        sliders = GetComponentsInChildren<Slider>().ToList();
+        sliders = GetComponentsInChildren<Slider>(includeInactive: true).ToList();
+        activeSlider = null;
+    }
+
+    private void Update()
+    {
+        if (activeSlider != null) 
+        {
+            Slider daSlider = activeSlider.GetComponent<Clickable>().slider;
+            RectTransform sliderPosition = daSlider.GetComponent<RectTransform>();
+
+            Vector3 playerOnScreen = levelCamera.WorldToScreenPoint(activeSlider.transform.position);
+
+            // The position is subtracted by a hard coded offset, feel free to make it more modular for different objects
+            sliderPosition.localPosition = playerOnScreen - new Vector3(396.2f, 216.4f, 0);
+        }
     }
 
     public void ActiveSlider(GameObject gameObject)
     {
         DisableAllSliders();
-        gameObject.GetComponent<Slider>().enabled = true;
+
+        activeSlider = gameObject;
+
+        gameObject.GetComponent<Clickable>().slider.gameObject.active = true;
     }
 
     private void DisableAllSliders()
     {
         foreach (Slider slider in sliders)
         {
-            slider.enabled = false;
+            slider.gameObject.active = false;
         }
     }
 
--- Assets/Scripts/Click Interactions/Clickable.cs
+++ Assets/Scripts/Click Interactions/Clickable.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Clickable : MonoBehaviour, IPointerClickHandler
 {
     public UnityEvent<GameObject> MouseClick;
+
+    public Slider slider;
 
     private void Awake()
     {
