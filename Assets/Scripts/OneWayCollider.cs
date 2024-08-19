a5eea64bbe5438cd3ea42e151f43885abc27e5d0
epellini <142680134+epellini@users.noreply.github.com>
2024-08-19 16:34:30 -0400

    Fix: OneWayCollider.cs

--- Assets/Scripts/OneWayCollider.cs
+++ Assets/Scripts/OneWayCollider.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class OneWayCollider : MonoBehaviour
 {
-    private BoxCollider solidCollider, triggerCollider;
+    //private BoxCollider solidCollider, triggerCollider;
+    private BoxCollider solidCollider;
+    private BoxCollider triggerCollider;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +18,30 @@ public class OneWayCollider : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        solidCollider.enabled = false;
+        // solidCollider.enabled = false;
+        if (IsPlayer(other) && IsPlayerAbove(other))
+        {
+            solidCollider.enabled = false;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        solidCollider.enabled = true;
+        //solidCollider.enabled = true;
+        if (IsPlayer(other))
+        {
+            solidCollider.enabled = true;
+        }
+    }
+
+    private bool IsPlayer(Collider other)
+    {
+        return other.CompareTag("Player");
+    }
+
+    private bool IsPlayerAbove(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        return rb != null && rb.velocity.y > 0;
     }
 }
